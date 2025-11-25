3bd2384a653bb7f7e1116591f86490a1a67242fb
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-25 21:29:08 +0500

    Index.js for in constants

--- mac-pro/src/App.jsx
+++ mac-pro/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Navbar} from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 const App = () => {
   return (
-    <div className=''>
-      App
+    <div>
+      <Navbar />
+      <Hero />
     </div>
   )
 }
