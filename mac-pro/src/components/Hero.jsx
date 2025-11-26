b4d529537042b7feb43e78545c59b3af3305d9b8
Muhammad Usman <usmanchaudhary112579@gmail.com>
2025-11-26 23:54:32 +0500

    Added a simple text and logo for today..

---
+++ mac-pro/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero'>
+      <div>
+        <h1>MacBook Pro</h1>
+        <img src="./title.png" alt="Title" />
+      </div>
+
+    </section>
+  )
+}
+
+export default Hero
